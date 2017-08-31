29386092555d1c0bfe308170a22f1cfdef6e7866
Anson Bridges <bridges.anson@gmail.com>
2017-08-31 09:57:21 -0700

    line draw here it is

--- render.c
+++ render.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 //definitions
 #define H 600
 #define W 800
@@ -128,25 +129,110 @@ int placePoint(int px, int py, rgbcolor color)
 {
     if (px < 0 || py < 0 || px > W-1 || py > H-1) return -1;
     int *pix = (int*) surface->pixels;
-    int clr = pow(16,2)*color.r + 16*color.g + color.b;
-    
+    int clr = pow(16,4)*color.r + pow(16,2)*color.g + color.b;
     pix[py*W+px] = clr;
     
     return 0;
 }
 
+int drawLine(int p1x, int p1y, int p2x, int p2y, rgbcolor color)
+{
+    if ( p1x < 0 || p1y < 0 || p2x < 0 || p2y < 0 ) return -1;
+    if ( p1x > W-1 || p2x > W-1 || p1y > H-1 || p2y > H-1 ) return -1;
+    
+    int d = 1;
+    int s = p1x;
+    int e = p2x;
+    if ( p1x > p2x ) { d = -1; s = p2x; e = p1x; }
+    
+    if ( p1x == p2x )
+    {
+        int h = abs(p1y-p2y);
+        if ( p1y > p2y )
+        {
+            for ( h = p2y; h <= p1y; h++)
+            {
+                placePoint(p1x, h, color);
+            }
+        }
+        else
+        {
+            for ( h = p1y; h <= p2y; h++)
+            {
+                placePoint(p1x, h, color);
+            }
+        }
+    }
+    else
+    {
+        int w;
+        
+        double dp1x = (double)p1x;
+        double dp2x = (double)p2x;
+        double dp1y = (double)p1y;
+        double dp2y = (double)p2y;
+        double m = (dp1y-dp2y)/(dp1x-dp2x);
+        double b = (double)(p1y - (m*p1x));
+        for ( w = s; w <= e; w+1)
+        {
+            int y  = (int)(w*m + b);
+            int ny = (int)((++w)*m + b);
+            int ns = y;
+            int ne = ny;
+            if (ny <= y) { ns = ny; ne = y; }
+            int h;
+            for(h = ns; h <= ne; h++)
+            {
+                printf("h: %d, w: %d\n", h, w);
+                placePoint(w, h, color);
+            }
+        }
+        sleep(10);
+    }
+}
+
+void drawScreen()
+{
+    rgbcolor c = {255,255,255};
+    drawLine(0,0,100,400,c);
+}
 
 //main func
 int main()
 {
-    vector pa = {2.0,3.0,0.0}; vector pb = {6.0f,3.0f,0.0f}; vector pc = {6,3,6}; vector pd = {2,3,6};
-    vector nm = {0,0,0}; rgbcolor cl = {255,255,255};
-    plane mainpln = { pa, pb, pc, pd, nm, cl };
-    line  mainln  = { {0,0,0}, {3,4,2} };
-    setVector(&mainpln);
-    vector intersection = getIntersection(mainpln, mainln);
-    printf("x: %f, y: %f, z: %f\n", intersection.x, intersection.y, intersection.z);
-    
-    
+    surface = SDL_SetVideoMode(W, H, 32, 0);
+
+    SDL_EnableKeyRepeat(150, 30);
+    SDL_ShowCursor(SDL_DISABLE);
+
+    int wsad[4]={0,0,0,0}, ground=0, falling=1, moving=0, ducking=0;
+    for(;;)
+    {
+        SDL_LockSurface(surface);
+        drawScreen();
+        SDL_UnlockSurface(surface);
+        SDL_Flip(surface);
+
+
+        SDL_Event ev;
+        while(SDL_PollEvent(&ev))
+        {
+            switch(ev.type)
+            {
+                case SDL_KEYDOWN:
+                case SDL_KEYUP:
+                    switch(ev.key.keysym.sym)
+                    {
+                        case 'q': goto done;
+                        default: break;
+                    }
+                    break;
+                case SDL_QUIT: goto done;
+            }
+        }
+        SDL_Delay(10);
+    }
+done:
+    SDL_Quit();
     return 0;
 }
